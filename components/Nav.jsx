c0d5950e4c7ca71995e9bb76730eaf618e7aa981
armagantas <65099385+armagantas@users.noreply.github.com>
2023-05-29 02:45:03 +0300

    Commit

---
+++ components/Nav.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import Image from "next/image";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
