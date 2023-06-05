ce6080f65581567326bc5933c0f8d0ef08a6785e
armagantas <65099385+armagantas@users.noreply.github.com>
2023-06-05 03:45:58 +0300

    Commit
    Creating post page has been created and added some features.

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
+
+import Form from "@components/Form";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 const CreatePrompt = () => {
-  return <div>CreatePrompt</div>;
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {};
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
